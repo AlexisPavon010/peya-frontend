50507d99dc02e87c41720037f5644ae594a00858
Alexis Pavon <themaster034@gmail.com>
2022-08-29 21:56:42 -0300

    home page added

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "../Components/Hero"
+import { Main } from "../Components/Main"
+
+export const HomePage = () => {
+    return (
+        <>
+        <Hero />
+        <Main />
+        </>
+    )
+}
